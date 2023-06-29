3a4b8d645a4ffc778fc300683d1561327db8fcd2
xcoder <117849398+xcoder-ksb@users.noreply.github.com>
2023-06-29 09:59:01 +0630

    Update and rename v2.py to fuck.py

--- fuck.py
+++ fuck.py
@@ -3,6 +3,6 @@ os.system('pip uninstall requests')
 os.system('pip install requests')
 x=platform.architecture()[0]
 if x =="64bit":
-     print("Pls Wait Bro Updating tool")
+     from main import gmail
 else:
      print("Your System doesn't support ")
